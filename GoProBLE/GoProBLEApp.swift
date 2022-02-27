50e99f2af1d99a2640fc66ed6193e0a252726377
khangzxrr <khangzxrr@gmail.com>
2022-02-27 21:34:21 +0700

    Initial Commit

---
+++ GoProBLE/GoProBLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoProBLEApp.swift
+//  GoProBLE
+//
+//  Created by Mac on 27/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct GoProBLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
