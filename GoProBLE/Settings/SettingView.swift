302097d904bc998d10c49c4a615b5e43517da9eb
khangzxrr <khangzxrr@gmail.com>
2022-04-20 10:23:58 +0700

    add setting

---
+++ GoProBLE/Settings/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  RemoteBLE
+//
+//  Created by Mac on 19/04/2022.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
---
+++ GoProBLE/Settings/SettingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SettingViewModel.swift
+//  RemoteBLE
+//
+//  Created by Mac on 20/04/2022.
+//
+
+import Foundation
+
+class SettingViewModel : ObservableObject {
+    
+}
